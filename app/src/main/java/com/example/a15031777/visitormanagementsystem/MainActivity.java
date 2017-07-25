e50b619ae1ac2b34a02479d633e574bf69afb7e8
actuallycoding <15017484@myrp.edu.sg>
2017-07-26 01:10:45 +0800

    Basic login works. Just need to direct to correct activities.

--- app/src/main/java/com/example/a15031777/visitormanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/a15031777/visitormanagementsystem/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.a15031777.visitormanagementsystem;
 
-import android.content.Intent;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class MainActivity extends AppCompatActivity {
     EditText etUName, etPw;
@@ -26,61 +32,56 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
         int userLoggedIn = pref.getInt("isLoggedIn", -1);
 
-//
-//        if (userLoggedIn == -1) {
-//            btnLogin.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//
-//                    SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-//                    SharedPreferences.Editor edit = pref.edit();
-//                    String username = etUName.getText().toString();
-//                    String pw = etPw.getText().toString();
-//                    Boolean userId = db.checkUser(username, pw);
-//
-//                    if (userId == true) {
-//                        int id = db.getUserId(username, pw);
-//                        edit.putInt("isLoggedIn", id).commit();
-//                        User currentUser = db.getUserWithId(id);
-//                        db.close();
-//                        String role = currentUser.getUserRole();
-//                        if (role.equalsIgnoreCase("Administrator")) {
-//                            Intent i = new Intent(MainActivity.this, AdminActivity.class);
-//                            startActivity(i);
-//                        } else if (role.equalsIgnoreCase("Host")) {
-//                            Intent i = new Intent(MainActivity.this, HostActivity.class);
-//                            startActivity(i);
-//                        } else if (role.equalsIgnoreCase("Manager")) {
-//                            Intent i = new Intent(MainActivity.this, ManagerActivity.class);
-//                            startActivity(i);
-//                        } else {
-//                            Intent i = new Intent(MainActivity.this, SecurityGuardActivity.class);
-//                            startActivity(i);
-//                        }
-//
-//                    } else {
-//                        Toast.makeText(MainActivity.this, "Your username/password is wrong.", Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//            });
-//        } else {
-//            DBHandlerUser db = new DBHandlerUser(MainActivity.this);
-//            User currentUser = db.getUserWithId(userLoggedIn);
-//            db.close();
-//            String role = currentUser.getUserRole();
-//            if (role.equalsIgnoreCase("Administrator")) {
-//                Intent i = new Intent(MainActivity.this, AdminActivity.class);
-//                startActivity(i);
-//            } else if (role.equalsIgnoreCase("Host")) {
-//                Intent i = new Intent(MainActivity.this, HostActivity.class);
-//                startActivity(i);
-//            } else if (role.equalsIgnoreCase("Manager")) {
-//                Intent i = new Intent(MainActivity.this, ManagerActivity.class);
-//                startActivity(i);
-//            } else {
-//                Intent i = new Intent(MainActivity.this, SecurityGuardActivity.class);
-//                startActivity(i);
-//            }
-//        }
+
+        if (userLoggedIn == -1) {
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String userName = etUName.getText().toString();
+                    String password = etPw.getText().toString();
+                    ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+                    if (networkInfo != null && networkInfo.isConnected()) {
+
+                        HttpRequest request = new HttpRequest("https://pyramidal-drift.000webhostapp.com/doLogin.php");
+                        request.addData("username", userName);
+                        request.addData("password", password);
+                        request.setMethod("POST");
+                        request.execute();
+
+                        /******************************/
+                        try {
+                            String jsonString = request.getResponse();
+                            Log.d("JsonString", "jsonString: " + jsonString);
+
+                            JSONObject jsonObj = (JSONObject) new JSONTokener(jsonString).nextValue();
+                            if (jsonObj.getBoolean("authenticated")) {
+                                // When authentication is successful
+                                String id = jsonObj.getString("user_id");
+                                String role = jsonObj.getString("user_role");
+                                Log.d("UserId", "userId: " + id);
+                                Log.d("UserRole", "userRole: " + id);
+                                SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                                SharedPreferences.Editor edit = pref.edit();
+                                edit.putInt("isLoggedIn", Integer.parseInt(id));
+                                edit.commit();
+//                                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+//                                startActivity(intent);
+                                Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                Toast.makeText(MainActivity.this, "Authentication failed, please login again", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Toast.makeText(MainActivity.this, "No network connection available.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+
+        }
     }
 }
