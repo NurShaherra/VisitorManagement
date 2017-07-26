e5d78a526a7ce12dd231cada8946cd04be2fc7e8
15031777 <15031777@myrp.edu.sg>
2017-07-26 12:31:53 +0800

    Updated display user info activity

--- app/src/main/java/com/example/a15031777/visitormanagementsystem/DisplayUserInfo.java
+++ app/src/main/java/com/example/a15031777/visitormanagementsystem/DisplayUserInfo.java
@@ -19,25 +19,29 @@ import java.util.ArrayList;
 
 public class DisplayUserInfo extends AppCompatActivity {
     Intent intent;
-    ArrayList<User> personList = new ArrayList<User>();
+    ArrayList<String> al;
+    ArrayAdapter<String> aa;
     ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.lvUser);
+        intent = getIntent();
     }
 
     public void onResume() {
         super.onResume();
-        personList.clear();
+        al.clear();
         // Check if there is network access
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
         if (networkInfo != null && networkInfo.isConnected()) {
 
             //helper class - parse in the http url
-            HttpRequest request = new HttpRequest("pyramidal-drift.000webhostapp.com/getAllUsers.php");
+            HttpRequest request = new HttpRequest("http://10.0.2.2/FYP/getAllUsers.php");
             //specify the method
             request.setMethod("GET");
             //call the webservice
@@ -53,37 +57,19 @@ public class DisplayUserInfo extends AppCompatActivity {
                 // Populate the arraylist personList
                 for (int i = 0; i < jsonArray.length(); i++) {
                     JSONObject jObj = jsonArray.getJSONObject(i);
-                    System.out.println(jObj.getString("firstname") + " " + jObj.getString("lastname"));
-                    User person = new User();
-                    person.setId(jObj.getInt("user_id"));
-                    person.setUsername(jObj.getString("user_name"));
-                    person.setEmail(jObj.getString("email_address"));
-                    person.setUserRole(jObj.getString("user_role"));
-                    person.setFullname(jObj.getString("full_name"));
-                    person.setUnitAddress(jObj.getString("unit_address"));
-                    person.setBlocknum(jObj.getString("block_number"));
-                    personList.add(person);
+                    al = new ArrayList<String>();
+                    aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al);
+                    al.add(jObj.getString("username"));
+
+                    listView.setAdapter(aa);
+                    aa.notifyDataSetChanged();
+
                 }
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
-            UserAdapter arrayAdapter = new UserAdapter(this, R.layout.rowuser, personList);
-            listView = (ListView) findViewById(R.id.lvUser);
-            listView.setAdapter(arrayAdapter);
-
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View arg1, int arg2, long arg3) {
-
-                    User person = (User) parent.getItemAtPosition(arg2);
-
-                    intent = new Intent(getApplicationContext(), MainActivity.class);
-                    intent.putExtra("com.example.MAIN_MESSAGE", Integer.toString(person.getId()));
-                    startActivity(intent);
-                }
-            });
         }
     }
 }
