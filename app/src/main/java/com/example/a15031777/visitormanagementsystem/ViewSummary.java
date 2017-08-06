675a51859760649394366812439be312fa8bbdfe
15031777 <15031777@myrp.edu.sg>
2017-08-06 20:57:32 +0800

    Added Admin Activity

---
+++ app/src/main/java/com/example/a15031777/visitormanagementsystem/ViewSummary.java
@@ -0,0 +1,13 @@
+package com.example.a15031777.visitormanagementsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_summary);
+    }
+}
