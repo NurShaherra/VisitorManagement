3943ed1e15ea2943ce4aca90e56c9b774a90c7cb
15031777 <15031777@myrp.edu.sg>
2017-08-07 23:42:59 +0800

    Made changes to display visitor info

--- app/src/main/java/com/example/a15031777/visitormanagementsystem/EditUser.java
+++ app/src/main/java/com/example/a15031777/visitormanagementsystem/EditUser.java
@@ -1,9 +1,13 @@
 package com.example.a15031777.visitormanagementsystem;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -77,5 +81,29 @@ public class EditUser extends AppCompatActivity {
                 }
             }
         });
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_host, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.menu_logout) {
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(EditUser.this);
+            SharedPreferences.Editor edit = pref.edit();
+            edit.putInt("isLoggedIn", -1).commit();
+            Intent i = new Intent(EditUser.this, MainActivity.class);
+            startActivity(i);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
