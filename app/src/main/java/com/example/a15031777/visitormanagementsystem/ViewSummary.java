758a88865950fff93e633dde00ef068cb3370a2f
15031777 <15031777@myrp.edu.sg>
2017-08-14 18:57:48 +0800

    changed view summary

--- app/src/main/java/com/example/a15031777/visitormanagementsystem/ViewSummary.java
+++ app/src/main/java/com/example/a15031777/visitormanagementsystem/ViewSummary.java
@@ -1,19 +1,111 @@
 package com.example.a15031777.visitormanagementsystem;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ViewSummary extends AppCompatActivity {
+
+    ListView lv;
+    Spinner spn;
+    ArrayList<String> al;
+    ArrayAdapter<String> aa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_summary);
+
+        Intent i = getIntent();
+
+        lv = (ListView) findViewById(R.id.lvSummary);
+        spn = (Spinner) findViewById(R.id.spinnerSummary);
+        al = new ArrayList<String>();
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.summary, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spn.setAdapter(adapter);
+
+        // Check if there is network access
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+
+            //helper class - parse in the http url
+            HttpRequest request = new HttpRequest("http://localhost/FYP/getSummary.php");
+            //specify the method
+            request.setMethod("GET");
+            //call the webservice
+            request.execute();
+
+            aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al);
+            lv.setAdapter(aa);
+
+            spn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    String selectedItem = parent.getItemAtPosition(position).toString();
+                    if (selectedItem.equalsIgnoreCase("Year")) {
+                        al.clear();
+
+                        //helper class - parse in the http url
+                        HttpRequest request = new HttpRequest("http://localhost/FYP/getSummary.php");
+                        //specify the method
+                        request.setMethod("GET");
+                        //call the webservice
+                        request.execute();
+
+                        al.clear();
+                        try {
+                            //get the response back
+                            String jsonString = request.getResponse();
+                            System.out.println(">>" + jsonString);
+
+                            //do something with json string
+                            JSONArray jsonArray = new JSONArray(jsonString);
+
+                            // Populate the arraylist personList
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jObj = jsonArray.getJSONObject(i);
+                                String year = jObj.getString("record_created_date");
+                                al.add(year);
+                            }
+
+                            aa.notifyDataSetChanged();
+
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+        }
+
     }
 
     @Override
